bdf1bb46e627aeee906dc7a7bd8b3432e8a62ba7
spsaran <spsaran12@yahoo.com>
2016-07-25 20:25:32 -0400

    Add files via upload

---
+++ DeDuplicate.java
@@ -0,0 +1,111 @@
+package com.ntt.data.util;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Removing duplicate element from the int array
+ * @author ssomu
+ *
+ */
+public class DeDuplicate {
+
+	Logger logger = Logger.getLogger(DeDuplicate.class.getName());
+	
+	public int[] randomIntegers = {1,2,34,34,25,1,45,3,26,85,4,34,86,25,43,2,1,10000,11,16,19,1,18,4,9,3,
+									20,17,8,15,6,2,5,10,14,12,13,7,8,9,1,2,15,12,18,10,14,20,17,16,3,6,19,
+									13,5,11,4,7,19,16,5,9,12,3,20,7,15,17,10,6,1,8,18,4,14,13,2,11};
+	
+	public static void main(String [] args) {
+		DeDuplicate dedup = new DeDuplicate();
+		dedup.removeDup(Arrays.copyOf(dedup.randomIntegers, dedup.randomIntegers.length)); //copy the original array
+		dedup.removeDupSet(dedup.randomIntegers);
+		dedup.removeDupLinkedSet(dedup.randomIntegers);
+	}
+	
+	/**
+	 * Removing the duplicate value from the integer array
+	 * @param arr
+	 * @return integer array
+	 */
+	public int[] removeDup(int[] arr) {
+		logger.log(Level.INFO, "DeDuplicate.removeDup:" + Arrays.toString(arr));
+		if ( null != arr ) {
+			int len = arr.length;
+			int[] newarr = new int[len];
+			Arrays.sort(arr); //quick sort
+			int latest = -1;
+			int i = 0;
+			for (int el: arr) {
+				if ( el != latest ) {
+					newarr[i++] = el;
+					latest = el;
+				}
+			}
+			newarr = Arrays.copyOf(newarr, i);
+			logger.log(Level.INFO, "DeDuplicate.removeDup:" + Arrays.toString(newarr));
+			return newarr;
+		}
+		return null;
+	}
+	
+	/**
+	 * Removing the duplicate value from the integer array using HashSet
+	 * @param arr
+	 * @return integer array
+	 */
+	public int[] removeDupSet(int[] arr) {
+		logger.log(Level.INFO, "DeDuplicate.removeDupSet:" + Arrays.toString(arr));
+		if ( null != arr ) {
+			int len = arr.length;
+			Set<Integer> hashSet = new HashSet<Integer>(len);
+			for (int i: arr) {
+				hashSet.add(i);
+			}
+			int[] newarr = convertPrimitive(hashSet);
+			logger.log(Level.INFO, "DeDuplicate.removeDupSet:" + Arrays.toString(newarr));
+			return newarr;
+		}
+		return null;
+	}
+	
+	/**
+	 * Removing the duplicate value from the integer array using LinkedHashSet to maintain the original order
+	 * @param arr
+	 * @return integer array
+	 */
+	public int[] removeDupLinkedSet(int[] arr) {
+		logger.log(Level.INFO, "DeDuplicate.removeDupLinkedSet:" + Arrays.toString(arr));
+		if ( null != arr ) {
+			int len = arr.length;
+			Set<Integer> hashSet = new LinkedHashSet<Integer>(len);
+			for (int i: arr) {
+				hashSet.add(i);
+			}
+			int[] newarr = convertPrimitive(hashSet);
+			logger.log(Level.INFO, "DeDuplicate.removeDupLinkedSet:" + Arrays.toString(newarr));
+			return newarr;
+		}
+		return null;
+	}
+	
+		
+	private int[] convertPrimitive(Collection<Integer> col) {
+		logger.info("convertPrimitive:col-"+col);
+		int[] ret = new int[col.size()];
+		int i = 0;
+		for(Integer el: col) {
+			ret[i++] = el;
+		}
+		return ret;
+	}
+	
+	public static void sop(String str) {
+		System.out.println(str);
+	}
+}
---
+++ DeDuplicateTest.java
@@ -0,0 +1,34 @@
+package com.ntt.data.util;
+
+import java.util.Arrays;
+
+import junit.framework.TestCase;
+
+public class DeDuplicateTest extends TestCase {
+	
+	DeDuplicate dedup;
+	int[] arr = {1, 10, 200, 7, 1, 9, 201, 5, 20, 1, 200, 10};
+	int[] dearr = {1, 5, 7, 9, 10, 20, 200, 201}; 
+	
+	protected void setUp(){
+		dedup = new DeDuplicate();
+   }
+
+	public void testRemoveDup() {
+		int[] newarr = dedup.removeDup(arr);
+		assertTrue(Arrays.equals(newarr, dearr));
+	}
+
+	public void testRemoveDupSet() {
+		int[] newarr = dedup.removeDupSet(arr);
+		Arrays.sort(newarr);
+		assertTrue(Arrays.equals(newarr, dearr));
+	}
+
+	public void testRemoveDupLinkedSet() {
+		int[] newarr = dedup.removeDupLinkedSet(arr);
+		Arrays.sort(newarr);
+		assertTrue(Arrays.equals(newarr, dearr));
+	}
+
+}
